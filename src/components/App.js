7bac20b0bc6de0ab61f2a0a18f9ac9e8bd068a95
Shubham5561 <shubham.vats.8199075579@gmail.com>
2022-08-08 13:45:21 +0530

    ready to add

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PostList from "./PostList";
+import App from "../style/App.css";
+
+const Apps = () => {
+    return (
+    <div className="ui container">
+        <PostList/>
+    </div>
+    );
+};
+
+export default Apps;
